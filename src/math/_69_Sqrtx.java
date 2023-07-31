c56cc3f65ea679a52bba99f51c818a06bb855a95
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-07-31 23:54:13 +0300

    69. Sqrt(x) binary search solution

--- src/math/_69_Sqrtx.java
+++ src/math/_69_Sqrtx.java
@@ -11,4 +11,20 @@ public class _69_Sqrtx {
         }
         return ans - 1;
     }
+
+    public int mySqrt_binSearch(int x) {
+        if(x == 0) return 0;
+        int l = 1, r = x;
+        while(true) {
+            int m =  l + (r-l)/2;
+            if(m > x / m) {
+                r = m - 1;
+            } else if (m + 1 > x / (m+1)) {
+                return m;
+            }
+            else {
+                l = m + 1;
+            }
+        }
+    }
 }
