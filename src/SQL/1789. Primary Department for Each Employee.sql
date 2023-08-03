928ea2f3d51f8c99592133d608d48a3c672beb62
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-08-03 23:15:58 +0300

    1789. Primary Department for Each Employee

---
+++ src/SQL/1789. Primary Department for Each Employee.sql
@@ -0,0 +1,13 @@
+SELECT
+    employee_id,
+    department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+UNION
+SELECT
+    employee_id,
+    department_id
+FROM Employee
+GROUP BY employee_id
+HAVING count(employee_id) = 1
+ORDER BY employee_id
