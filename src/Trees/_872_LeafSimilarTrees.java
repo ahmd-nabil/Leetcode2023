598dc970edb8c10e1c01e85f9085a041d7b9379d
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-08-25 20:32:55 +0300

    872. Leaf-Similar Trees

---
+++ src/Trees/_872_LeafSimilarTrees.java
@@ -0,0 +1,28 @@
+package Trees;
+
+import shared.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ahmed Nabil
+ */
+public class _872_LeafSimilarTrees {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leaves1 = new ArrayList<>();
+        List<Integer> leaves2 = new ArrayList<>();
+        dfs(root1, leaves1);
+        dfs(root2, leaves2);
+        return leaves1.equals(leaves2);
+    }
+
+    private void dfs(TreeNode root, List<Integer> leaves) {
+        if(root == null) return;
+        if(root.left == null && root.right == null) leaves.add(root.val);
+        else {
+            dfs(root.left, leaves);
+            dfs(root.right, leaves);
+        }
+    }
+}
