dce52f97b112a2937f57782b2e6e1af4113e5c19
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-09-07 13:46:33 +0300

    35. Search Insert Position

--- src/BinarySearch/_35_SearchInsertPosition.java
+++ src/BinarySearch/_35_SearchInsertPosition.java
@@ -1,4 +1,18 @@
-package BinarySearch;/**
+package BinarySearch;
+
+/**
  * @author Ahmed Nabil
- */public class _35_SearchInsertPosition {
+ */
+public class _35_SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        int m = (r - l) / 2 + l;
+        while(l < r) {
+            if(target == nums[m]) return m;
+            if(target > nums[m]) l = m + 1;
+            else r = m;
+            m = (r - l) / 2 + l;
+        }
+        return target > nums[m] ? m+1 : m;
+    }
 }
